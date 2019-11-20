d940424c9865f12a5b607b48ae66983b116ef054
Rodrigo <cuitlagua@gmail.com>
2019-11-19 23:33:01 -0600

    settime out issue

---
+++ callbacks/callBackNotes.md
@@ -0,0 +1,9 @@
+# CALLBACKS
+## función de orden superior
+Es una función que se pasa atraves de otra función.
+Una funcion que recibe otra función como parámetro es una función de orden superior.
+
+Es una función que se ejecuta después de que la función que la llama es llamada.
+
+por ejemplo foreach, ejecuta algo por cada item de un arreglo, se le pasa como parámetro una función.
+
---
+++ callbacks/callbacks.js
@@ -0,0 +1,61 @@
+function saludar(name, callback, secondCallback) {
+    // llamar algo que no existe, aquí se llama la función
+    console.log("Antes de callback");
+    callback(name);
+    console.log("después de callback");
+    secondCallback(name)
+    console.log("after 2nd callback name: " + name);
+}
+
+function saludo(name) {
+    function myFunc(num) {
+        if (num <= 0) {
+            return console.log("finished")
+        }
+        console.log("Que show " + name + " | . _ . |")
+        myFunc(num - 1)
+    }
+    return myFunc(100)
+}
+saludar("Yeyo Arris", // Aquí solo estamos pasando la definición de la función
+saludo, function(name) {
+    console.log("|> 1rst Good bye " + name + " ( * _ * )");
+    var setTime = setTimeout(function() {
+        console.log("|>setTimeOut Good bye " + name + " ( * _ * )");
+    }, 2000);
+    console.log("|> last Good bye " + name + " ( * _ * )");
+    return setTime
+});
+
+//STACK: 1.- Callback 2.-
+
+/*
+// callbacks como funciones nombradas
+
+// variable global
+
+var allUsers = [];
+
+// funcion de logueo (console.logs) genérica
+
+function logStuff(data) {
+  if ("string" == typeof data) return console.log(data);
+  if ("object" == typeof data) {
+    for (var key in data) {
+      console.log(key + ":" + data[key]);
+    }
+  }
+}
+
+// Una función recibe dos parametros; el segundo es un callback
+// 'getInput' no le importa que función le van a pasar como callback
+function getInput(input, callback) {
+  allUsers.push(input);
+  "function" == typeof callback && callback(input);
+}
+
+// Llamamos a 'getInput' con 'logStuff' como parametro
+// Así 'LogStuff' era ejecutada ('callBack') dentro de la función 'getInput'
+
+getInput({ user: "gvaliron", speciality: "javascript" }, logStuff);
+*/
