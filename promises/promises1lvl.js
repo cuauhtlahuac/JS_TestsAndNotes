a87d0c103e12db8bc95ed8a655152e72f9f0e9ca
Rodrigo <cuitlagua@gmail.com>
2019-11-20 18:42:05 -0600

    first promise reject

--- promises/promises1lvl.js
+++ promises/promises1lvl.js
@@ -4,19 +4,23 @@
 
 let promiseToCleanTheRoom = new Promise(function(res, rej) {
   // Cleaning the room
-  let isClean = true;
+  let isClean = false; // Cambiar el valor de esta variable para que sea reject or response
 
   if (isClean) {
     // el argumento que se le da a res va a ser el mismo que reciba la función callback cuando se ejecute el metodo then
     // en este caso es fromResolve
     res("Clean");
   } else {
-    rej();
+    rej("not Clean");
   }
 });
 
 // llamamos la funcion con el metodo then que se ejecutará después de que la promesa fue realizada.
 // Se le vuelve a pasar una función callback a then en cas de response.
-promiseToCleanTheRoom.then(function(fromResolve) {
-  console.log("The room is " + fromResolve);
-});
+promiseToCleanTheRoom
+  .then(function(fromResolve) {
+    console.log("The room is " + fromResolve);
+  })
+  .catch(fromReject => {
+    console.log("The room is " + fromReject);
+  });
