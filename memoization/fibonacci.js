184970e20753c723ea3fa438d5d6d1673b0024bb
Rodrigo <cuitlagua@gmail.com>
2019-11-23 19:12:25 -0600

    awsome speed test

--- memoization/fibonacci.js
+++ memoization/fibonacci.js
@@ -2,9 +2,18 @@
 /*
 La sucesión comienza con los números 0 y 1,2​ a partir de estos, «cada término es la suma de los dos anteriores
 */
-function fib(n) {
-	if (n <= 2) return 1;
-	return fib(n - 1) + fib(n - 2);
+function fib(n, prevValue = []) {
+	if (prevValue[n] != null) {
+		return prevValue[n];
+	}
+	let result;
+	if (n <= 2) {
+		return 1;
+	} else {
+		result = fib(n - 1, prevValue) + fib(n - 2, prevValue);
+	}
+	prevValue[n] = result;
+	return result;
 }
 
-console.log(fib(2))
+console.log(fib(500));
