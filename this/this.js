1eef1433f990cb3b57e4084bd93279a35dc8de94
Rodrigo <cuitlagua@gmail.com>
2021-08-20 16:51:49 -0500

    JSTN: this note

--- this/this.js
+++ this/this.js
@@ -2,21 +2,29 @@
 This es el objeto de javascript en el cual se está ejecutando el código actual
 This sólo existe., no se puede declarar como una variable o algo parecido
 */
-// This se inicializa en este contexto como un objeto vacío
+
+// This se inicializa en el contexto que s le llame, en este caso es un objeto vacío
 console.log('Esto es this =>', this);
+
+// Al asinarle un valor a this, se guarda y se creó una ueva propiedad en el objeto this.
 this.newProp = 'Una nueva prop para this';
 console.log(this);
 
-// Aquí encapsulamos cosas, y por nde creamos un nuevo contexto
+// Aquí encapsulamos cosas en un objeto, y por esa razón creamos un nuevo contexto
 const playa = {
 	palmeras: 50,
 	countPalmeras: function() {
 		return (
-			'en la playa hay ' + this.palmeras + ' palmeras + ' + playa.palmeras + ' otra forma de llamar a palmeras'
+			'en la playa hay ' +
+			this.palmeras +
+			// la línea debajo es otra forma de llamar a el número palmeras
+			' tambien en la playa hay  ' +
+			playa.palmeras +
 		);
 	}
 };
 console.log({ playa });
+
 console.log(playa.countPalmeras());
 const countPalmeras = playa.countPalmeras;
 console.log({ countPalmeras });
@@ -27,9 +35,11 @@ console.log(countPalmeras());
 bind
 call
 apply
-*/
+*/ß
+
 // Con Bind tienes que pasarle el contexto como parametro
 // se guarda en una variable para poder ejecutar
+
 const countBindeado = countPalmeras.bind(playa);
 console.log('Con Bind =>', countBindeado());
 // Con metodo Call evitamos guardar la funcion en una variable para despues ejecutarla
